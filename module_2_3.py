1322b765a4f6092fe4aae09ad66f7e3c7524781e
Rezinkampf <rezinkampf@gmail.com>
2024-12-18 20:13:44 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,16 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_list):
+    if my_list[a] < 0:
+        break
+    poloj = my_list[a]
+    print(poloj)
+    a += 1
+
+
+
+
+
+
+
+
